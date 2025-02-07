bb52abe0b88eb4ef01ab6292afe3653ff90b6585
bastien707 <bastien.facqueur@efrei.net>
2025-02-07 17:38:17 +0100

    feat: react is very cool
    Revert "fixed previous"
    
    This reverts commit 5be4759cd3d5b702436b40384560717eb95f2292.
    
    feat: react is damn cool
    
    feat: react is damn cool
    
    feat: remove react logo
    
    feat: add title
    
    Revert "feat: react is damn cool"
    
    This reverts commit f6c8f1fb73494c7182f60b47178380a54e8ca151.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,12 +12,11 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <div>test</div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>My React app is nice !</h1>
+      <h1>My React app is awesome!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 3)}>
           count is {count}
@@ -26,9 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite logos to learn more</p>
     </>
   );
 }
